9cd938c1f71edacde0b483a48438287f53a78fc9
lookinALX <lookin.alx@gmail.com>
2024-10-07 22:03:06 +0200

    getting creation time of the file

--- getFileInfoForWindows.cpp
+++ getFileInfoForWindows.cpp
@@ -1,11 +1,50 @@
 #include <iostream>
+#include <stdio.h>
 #include <windows.h>
 
-string GetFileInfo(string filePath){
-    HANDLE hFile = CreateFile(filePath, GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, 0, NULL);
+
+void PrintFileTime(const FILETIME& ft) {
+    SYSTEMTIME stUTC, stLocal;
+    FileTimeToSystemTime(&ft, &stUTC);
+    SystemTimeToTzSpecificLocalTime(NULL, &stUTC, &stLocal);
+
+    std::cout << stLocal.wYear << std::endl;
 }
 
-int main(int argc, char *argv[]) {
-    GetFileInfo("1234");
+
+std::string GetFileInfo(const char* filePath)
+{
+    HANDLE hFile; 
+    hFile = CreateFile(filePath,                     // name of the write
+                    GENERIC_WRITE,                   // open for write
+                    FILE_SHARE_WRITE,                // share write
+                    NULL,                            // default security
+                    OPEN_EXISTING,                   // create new file only
+                    FILE_ATTRIBUTE_NORMAL,           // normal file
+                    NULL);                           // no attr. template
+    
+    if (hFile == INVALID_HANDLE_VALUE) 
+    { 
+        printf("Terminal failure: Unable to open file \"%s\".\n", filePath);
+        return NULL;
+    }   
+
+    FILETIME creationTime, accessTime, writeTime;
+    if (GetFileTime(hFile, &creationTime, &accessTime, &writeTime)) {
+        std::cout << "Creation Time: ";
+        PrintFileTime(creationTime);
+    } else {
+        std::cerr << "Error retrieving file time!" << std::endl;
+    }
+
+    // Close the file handle
+    CloseHandle(hFile);
+    return "";
+}
+
+int main(int argc, char *argv[]) 
+{
+    GetFileInfo("H:\\projects\\YearlyMediaSorter\\testdata\\filetomove.jpg");
+    return 0;
 }
 
